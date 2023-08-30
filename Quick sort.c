6eabdbd4e756472f0550f9ca67832466c1dac3fe
Harish-123456787 <124224081+Harish-123456787@users.noreply.github.com>
2023-08-30 12:34:25 +0530

    Quick sort.c

---
+++ Quick sort.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+// Swap two elements
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Partitioning the array and placing the pivot element in its correct position
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high]; // Pivot element
+    int i = (low - 1); // Index of smaller element
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+// Quick Sort function
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array:\n");
+    for (int i = 0; i < arr_size; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    quickSort(arr, 0, arr_size - 1);
+
+    printf("\nSorted array:\n");
+    for (int i = 0; i < arr_size; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
